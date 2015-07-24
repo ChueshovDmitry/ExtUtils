13bd517ee8b2ebee3b2155473a6c8175e1466e40
Kondakov A <skynet1331@mail.ru>
2015-07-24 16:20:35 +0500

    обработка эксепшена, метод для форматирования xml

--- src/main/java/com/mycomp/ExtUtils/UParser.java
+++ src/main/java/com/mycomp/ExtUtils/UParser.java
@@ -1,5 +1,7 @@
 package com.mycomp.ExtUtils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.StringReader;
 import java.net.URL;
@@ -7,6 +9,11 @@ import java.net.URL;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -30,34 +37,39 @@ private static String errorTrace;
 	{
 		super();
 	}
-	
-	public UParser(String xml, boolean isFile) throws Exception {
+
+	public UParser(String xml, boolean isFile) {
 		super();
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		factory.setIgnoringComments(true);
-		factory.setCoalescing(true); // Convert CDATA to Text nodes
-		factory.setNamespaceAware(false); // No namespaces: this is default
-		factory.setValidating(false);
-		DocumentBuilder parser = factory.newDocumentBuilder();
-		Document doc = null;
-		if(isFile){
-			File fileXml = new File(xml);
-			if(!fileXml.exists()){
-				errorMessge = "Ошибка: Файл "+xml+" не существует!";
-				System.out.println(errorMessge);
-				return;
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setIgnoringComments(true);
+			factory.setCoalescing(true); // Convert CDATA to Text nodes
+			factory.setNamespaceAware(false); // No namespaces: this is default
+			factory.setValidating(false);
+			DocumentBuilder parser = factory.newDocumentBuilder();
+			Document doc = null;
+			if(isFile){
+				File fileXml = new File(xml);
+				if(!fileXml.exists()){
+					errorMessge = "Ошибка: Файл "+xml+" не существует!";
+					System.out.println(errorMessge);
+					return;
+				}
+				doc = parser.parse(fileXml);
+				rootel = doc.getDocumentElement(); // корневой элемент документа//
+			}else{
+				StringReader streamXml = new StringReader(xml); // создаем поток
+				InputSource fileXml = new InputSource();
+				fileXml.setCharacterStream(streamXml);
+				fileXml.setEncoding("UTF-8");
+				DocumentBuilder xmldoc;
+				xmldoc = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+				doc = xmldoc.parse(fileXml);
+				rootel = doc.getDocumentElement(); // корневой элемент документа//
 			}
-			doc = parser.parse(fileXml);
-			rootel = doc.getDocumentElement(); // корневой элемент документа//
-		}else{
-			StringReader streamXml = new StringReader(xml); // создаем поток
-			InputSource fileXml = new InputSource();
-			fileXml.setCharacterStream(streamXml);
-			fileXml.setEncoding("UTF-8");
-			DocumentBuilder xmldoc;
-			xmldoc = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			doc = xmldoc.parse(fileXml);
-			rootel = doc.getDocumentElement(); // корневой элемент документа//
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 
@@ -144,6 +156,30 @@ private static String errorTrace;
 		}
 
 		return result;
+	}
+
+	/**
+	 * Выстраивает xml лесенкой
+	 * @param xml - строка xml
+	 * @return возвращает отформатированную xml
+	 */
+	public static String formatXml(String xml){
+		try{
+			Transformer serializer= SAXTransformerFactory.newInstance().newTransformer();
+			serializer.setOutputProperty("encoding", "utf-8");
+			serializer.setOutputProperty("indent", "yes");
+			serializer.setOutputProperty(
+					"{http://xml.apache.org/xslt}indent-amount",
+					"4");
+
+			Source xmlSource=new SAXSource(new InputSource(new ByteArrayInputStream(xml.getBytes())));
+			StreamResult res =  new StreamResult(new ByteArrayOutputStream());
+			serializer.transform(xmlSource, res);
+			return new String(((ByteArrayOutputStream)res.getOutputStream()).toByteArray());
+		}catch(Exception e){
+			//TODO log error
+			return xml;
+		}
 	}
 	
 }
